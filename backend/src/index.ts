9c59fc14e22a4c9d4e642bc589fcc80e77dedf51
kunstewi <kan.ai35423@gmail.com>
2025-11-20 18:16:48 +0530

    decided to not use mongodb and use postgres

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./config/db");
 
 // express instance
 const app = express();
@@ -17,9 +16,6 @@ app.use(
 
 // middleware to parse json
 app.use(express.json());
-
-// connect db
-connectDB();
 
 // Server Start
 const PORT = process.env.PORT || 5000;
