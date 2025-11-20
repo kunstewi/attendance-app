c3723c33617328256749d1ffb7e324bb25cf223c
kunstewi <kan.ai35423@gmail.com>
2025-11-20 12:01:20 +0530

    added mongodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,26 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+// express instance
+const app = express();
 
 // middleware to handle cors
+app.use(
+  cors({
+    origin: "*",
+    method: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// middleware to parse json
+app.use(express.json());
+
+// connect db
+connectDB();
+
+// Server Start
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
